facc7ec96bdbb6207b92f10312626fe4aa4e4fd2
Junior Albuquerque <junioralbuquerque81@gmail.com>
2022-07-13 22:13:03 -0400

    fix: package config

--- src/getLinkData.ts
+++ src/getLinkData.ts
@@ -3,7 +3,7 @@ import { PreviewLink } from "./utils/parseHtml/types"
 import { getPreviewData } from "./utils/previewTags"
 import { getPageContent } from "./utils/scrap"
 
-const getLinkData = async (url: string): Promise<PreviewLink> => {
+const getLinkData = async (url: string): Promise<PreviewLink | undefined> => {
   try {
     if (!url.includes('https://')) {
       throw new Error('invalid url')
