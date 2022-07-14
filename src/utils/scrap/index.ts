57e3baeda15c9380ecef77975b386cbd1cca251e
Junior Albuquerque <junioralbuquerque81@gmail.com>
2022-07-13 23:44:21 -0400

    fix: acces allow origin

--- src/utils/scrap/index.ts
+++ src/utils/scrap/index.ts
@@ -1,7 +1,13 @@
 import axios from 'axios'
 
 const getPageContent = async (url: string) => {
-  const { data } = await axios.get(url)
+  const { data } = await axios.get(url, {
+    method: 'get',
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Content-Type': 'application/json',
+    }
+  })
 
   return data
 }
