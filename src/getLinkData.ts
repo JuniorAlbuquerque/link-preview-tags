db5892b8f290926d0310914bfcd2292d63e66bb4
Junior Albuquerque <junioralbuquerque81@gmail.com>
2022-07-13 22:28:02 -0400

    fix: import types

--- src/getLinkData.ts
+++ src/getLinkData.ts
@@ -1,11 +1,17 @@
 import { parseHtml } from "./utils/parseHtml"
-import { PreviewLink } from "./utils/parseHtml/types"
 import { getPreviewData } from "./utils/previewTags"
 import { getPageContent } from "./utils/scrap"
 
+type PreviewLink = {
+  description: string
+  title: string
+  image: string
+  url: string
+}
+
 const getLinkData = async (url: string): Promise<PreviewLink | undefined> => {
   try {
-    if (!url.includes('https://')) {
+    if (!url?.includes('https://')) {
       throw new Error('invalid url')
     }
 
